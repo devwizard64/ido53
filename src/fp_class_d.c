aacc5e5bc4a3cbf6cd43d0516f33cb153b708132
devwizard <53394095+devwizard64@users.noreply.github.com>
2024-05-18 04:44:18 -0700

    fix fp_class_d

--- src/fp_class_d.c
+++ src/fp_class_d.c
@@ -2,10 +2,10 @@
 
 void lib_fp_class_d(CPU *cpu)
 {
-	int x = f12.i[0^IX] >> 20 & 0x800;
-	int y = f12.i[0^IX] >> 20 & 0x7FF;
-	int z = f12.i[0^IX] & 0xFFFFF;
-	int w = f12.i[1^IX];
+	int x = f12.i[1^IX] >> 20 & 0x800;
+	int y = f12.i[1^IX] >> 20 & 0x7FF;
+	int z = f12.i[1^IX] & 0xFFFFF;
+	int w = f12.i[0^IX];
 	if (y == 0x7FF)
 	{
 		if (z || w) v0 = !(z & 0x80000) ? 1 : 0;
