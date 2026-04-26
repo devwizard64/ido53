aad2d9e611d1e416d479c40e217af16d177b42dc
devwizard64 <devwizard64@gmail.com>
2026-04-26 00:10:25 -0700

    fix sprintf bug

--- src/sprintf.c
+++ src/sprintf.c
@@ -10,6 +10,6 @@ int lib_vsprintf(PTR s, PTR format, PTR arg)
 	stream._flag = IOFBF|IOWRT;
 	stream._file = -1;
 	n = lib__doprnt(format, arg, &stream);
-	*cpu_s8(s+n++) = '\0';
+	*cpu_s8(stream._ptr) = '\0';
 	return n;
 }
